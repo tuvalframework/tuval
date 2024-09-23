f4a5099a32754d5f971a23d24f79e931cfb674f1
selimtan <stanoncloud@gmail.com>
2024-09-23 16:11:54 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './App/App';
 export * from './App/express/ExpressApp';
-
+export * from './App/express/ExpressRequest';
+export * from './App/express/ExpressResponse';
 export * from './Storage/Device/Local';
