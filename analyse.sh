32d6461bb1c0356bf11464057c5f9a8fc5f22283
selimtan <stanoncloud@gmail.com>
2024-10-04 10:47:42 +0300

    .

---
+++ analyse.sh
@@ -0,0 +1,5 @@
+/Users/selimtan/Downloads/sonar-scanner-6.2.1.4610-macosx-aarch64/bin/sonar-scanner \
+  -Dsonar.projectKey=Test \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.token=sqp_8a332dbf44768926e307758ed629cd7ae4da3200
